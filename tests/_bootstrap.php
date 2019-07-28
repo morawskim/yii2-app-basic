61c51c2ec8675884bb16543d457e98d93fdeac6a
Marcin Morawski <marcin@morawskim.pl>
2019-07-28 10:55:37 +0200

    Use dotenv in codeception bootstrap file

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,6 +1,10 @@
 <?php
-define('YII_ENV', 'test');
-defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ .'/../vendor/autoload.php';
+
+$dotenv = new \Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
